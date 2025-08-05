845d220b541e4bce565d7ac169b1c97f92ffe09e
erfanghasemi1 <erfanerfanghasemi1@gmail.com>
2025-08-05 14:46:52 +0330

    1283A

---
+++ 1283A.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int.TryParse(Console.ReadLine(), out int t);
+        int TotalTime = 0;
+        int[] results = new int[t];
+
+        for (int i = 0; i < t; i++)
+        {
+            int[] time = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            TotalTime += (60 - time[1]);
+
+            time[0]++;
+
+            TotalTime += ((24 - time[0]) * 60);
+            results[i] = TotalTime;
+            TotalTime = 0;
+        }
+
+        foreach(int i in results) Console.WriteLine(i);
+    }
+}
