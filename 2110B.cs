34909330a3ae9d19a180ddba9e7f57ace3174f94
erfanghasemi1 <erfanerfanghasemi1@gmail.com>
2025-08-06 02:21:23 +0330

    2110B

---
+++ 2110B.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        int.TryParse(Console.ReadLine(), out var t);
+
+        List<StringBuilder> results = Enumerable.Repeat(0, t).Select(_ => new StringBuilder()).ToList();
+
+        for (int i = 0; i < t; i++)
+            results[i].Append(Solution());
+
+        foreach(StringBuilder result in results) Console.WriteLine(result);
+    }
+
+    public static string Solution()
+    {
+        string input = Console.ReadLine()!;
+
+        int sum = 0;
+
+        for (int i = 1; i < input.Length - 1; i++)
+        {
+            if (input[i] == '(') sum++;
+            else sum--;
+
+            if (sum < 0) return "YES";
+        }
+        return "NO";
+    }
+}
