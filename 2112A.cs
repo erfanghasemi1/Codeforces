f362a1cabe725aff2079405db7ae9430dbce0920
erfanghasemi1 <erfanerfanghasemi1@gmail.com>
2025-08-07 01:47:27 +0330

    2112A

---
+++ 2112A.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Xml.Schema;
+
+public class Program
+{
+    public static void Main()
+    {
+        int.TryParse(Console.ReadLine(), out int t);
+
+        for (int i = 0; i < t; i++)
+            Solution();
+    }
+
+    public static void Solution()
+    {
+        int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray(); 
+
+        int a = inputs[0] , x = inputs[1] , y = inputs[2];
+
+        if ( (a >= x  && a <= y) || (a >= y && a<=x) ) Console.WriteLine("NO");
+
+        else Console.WriteLine("YES");
+    }
+}
