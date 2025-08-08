8a55167462bdda994a484014691f9ef55c2dac3e
erfanghasemi1 <erfanerfanghasemi1@gmail.com>
2025-08-08 20:42:20 +0330

    2128C

---
+++ 2128C.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int.TryParse(Console.ReadLine(), out int t);
+
+        for (int i = 0; i < t; i++)
+            Solution();
+    }
+
+    public static void Solution()
+    {
+        int.TryParse(Console.ReadLine(), out int n);
+
+        int[] b = Console.ReadLine()!.Split().Select(int.Parse).ToArray() ;
+        int minimum = b[0];
+        
+        for (int i = 1; i < n; i++)
+        {
+            if (2 * minimum <= b[i])
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            minimum = b[i] < minimum ? b[i] : minimum;
+        }
+        Console.WriteLine("YES");
+    }
+}
